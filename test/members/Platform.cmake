597d7bf2f3e3e1328f609ae6944f2a24ddfb0f1a
mat <26722564+matcool@users.noreply.github.com>
2024-01-03 17:08:43 -0300

    fix Platform.cmake

--- test/members/Platform.cmake
+++ test/members/Platform.cmake
@@ -19,3 +19,5 @@ if (NOT DEFINED GEODE_TARGET_PLATFORM)
 		message(FATAL_ERROR "Unable to detect platform, please set GEODE_TARGET_PLATFORM in the root CMake file.")
 	endif()
 endif()
+
+set(GEODE_TARGET_PLATFORM GEODE_TARGET_PLATFORM PARENT_SCOPE)
