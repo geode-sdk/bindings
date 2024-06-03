71405a0449a382cdd157ef700d5cfca0db8e8f19
SabeMP <sabemasterprox@gmail.com>
2024-06-02 23:16:03 -0300

    more tests, GJGameState should be 8 bytes bigger than it currently is

--- test/members/Windows.cpp
+++ test/members/Windows.cpp
@@ -6,6 +6,9 @@ using namespace geode::prelude;
 
 GEODE_SIZE_CHECK(GJBaseGameLayer, 0x36b0);
 GEODE_MEMBER_CHECK(GJBaseGameLayer, m_gameState, 0x1a8);
+GEODE_MEMBER_CHECK(GJBaseGameLayer, m_level, 0x870);
+
+GEODE_SIZE_CHECK(GJGameState, 0x6C8);
 
 GEODE_SIZE_CHECK(TableViewCell, 0x230);
 
