ee56933e350d99ed8f060a72818f6142d3c46611
SabeMP <sabemasterprox@gmail.com>
2024-06-02 23:11:34 -0300

    Initial tests for GJBaseGameLayer (it should be 1320 bigger than rn)

--- test/members/Windows.cpp
+++ test/members/Windows.cpp
@@ -4,6 +4,9 @@
 
 using namespace geode::prelude;
 
+GEODE_SIZE_CHECK(GJBaseGameLayer, 0x36b0);
+GEODE_MEMBER_CHECK(GJBaseGameLayer, m_gameState, 0x1a8);
+
 GEODE_SIZE_CHECK(TableViewCell, 0x230);
 
 #endif
