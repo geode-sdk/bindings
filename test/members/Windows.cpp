5d262f14e94a293c919ce3a89ac81cc83217fdb2
sleepyut <176715524+sleepyut@users.noreply.github.com>
2024-07-27 21:26:50 +0300

    playerobject (#677)

--- test/members/Windows.cpp
+++ test/members/Windows.cpp
@@ -233,7 +233,7 @@ GEODE_MEMBER_CHECK(EffectGameObject, m_ignoreGroupParent, 0x738);
 
 GEODE_SIZE_CHECK(PlayerObject, 0xc48);
 GEODE_MEMBER_CHECK(PlayerObject, m_mainLayer, 0x558);
-GEODE_MEMBER_CHECK(PlayerObject, m_unk4e4, 0x5a8);
+GEODE_MEMBER_CHECK(PlayerObject, m_maybeLastGroundObject, 0x5a8);
 GEODE_MEMBER_CHECK(PlayerObject, m_particleSystems, 0x690);
 GEODE_MEMBER_CHECK(PlayerObject, m_ghostTrail, 0x730);
 GEODE_MEMBER_CHECK(PlayerObject, m_swingFireMiddle, 0x780);
@@ -248,7 +248,7 @@ GEODE_MEMBER_CHECK(PlayerObject, m_hasCustomGlowColor, 0x980);
 GEODE_MEMBER_CHECK(PlayerObject, m_isLocked, 0xa2a);
 GEODE_MEMBER_CHECK(PlayerObject, m_lastGroundedPos, 0xa2c);
 GEODE_MEMBER_CHECK(PlayerObject, m_isPlatformer, 0xb70);
-GEODE_MEMBER_CHECK(PlayerObject, m_unk910, 0xb98);
+GEODE_MEMBER_CHECK(PlayerObject, m_jumpPadRelated, 0xb98);
 GEODE_MEMBER_CHECK(PlayerObject, m_robotFire, 0xc10);
 GEODE_MEMBER_CHECK(PlayerObject, m_gameLayer, 0xc20);
 GEODE_MEMBER_CHECK(PlayerObject, m_actionManager, 0xc30);
