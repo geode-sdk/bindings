db82a95514e550e3ba51be9925965a0b071fbeb2
Justin Pridgen <justincpridgen672@gmail.com>
2024-08-27 20:00:14 -0400

    oh my goodness gracious

--- scripts/ghidra/ScriptWrapper.java
+++ scripts/ghidra/ScriptWrapper.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import ghidra.app.script.GhidraScript;
