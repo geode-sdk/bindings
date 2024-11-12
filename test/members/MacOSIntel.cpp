9a652b722996fccefe1554bbdcfc9d2185be0b4e
Justin Pridgen <justincpridgen672@gmail.com>
2024-11-12 08:51:57 -0500

    update playlayer member tests

--- test/members/MacOSIntel.cpp
+++ test/members/MacOSIntel.cpp
@@ -37,7 +37,7 @@ GEODE_MEMBER_CHECK(PlayLayer, m_coinArray, 0x3508);
 GEODE_MEMBER_CHECK(PlayLayer, m_unkSomeFloat, 0x35a4);
 GEODE_MEMBER_CHECK(PlayLayer, m_hasCompletedLevel, 0x368D);
 GEODE_MEMBER_CHECK(PlayLayer, m_endLayerStars, 0x3694);
-GEODE_MEMBER_CHECK(PlayLayer, m_isPaused, 0x36F7);
+GEODE_MEMBER_CHECK(PlayLayer, m_isPaused, 0x36FF);
 
 // // GameManager
 // GEODE_MEMBER_CHECK(GameManager, m_playLayer, 0x180);
