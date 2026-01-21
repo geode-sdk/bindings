89cedd0ed0f217fa028535d5293e9559b90aa9f0
altalk23 <45172705+altalk23@users.noreply.github.com>
2026-01-21 03:34:37 +0300

    cccontrolcolorpicker fix x2

--- bindings/2.208/inline/CCControlColourPicker.cpp
+++ bindings/2.208/inline/CCControlColourPicker.cpp
@@ -10,17 +10,16 @@ cocos2d::extension::CCControlColourPicker::CCControlColourPicker() {
 
 cocos2d::extension::CCControlColourPicker::~CCControlColourPicker() {}
 
-#if defined(GEODE_IS_WINDOWS) || defined(GEODE_IS_IOS)
+#if defined(GEODE_IS_ANDROID) || defined(GEODE_IS_MAC) || defined(GEODE_IS_IOS)
+cocos2d::ccColor3B const& cocos2d::extension::CCControlColourPicker::getColorValue() const  {
+    return m_rgb;
+}
 #endif
 
 #if defined(GEODE_IS_WINDOWS)
 #endif
 
 #if defined(GEODE_IS_IOS)
-cocos2d::ccColor3B const& cocos2d::extension::CCControlColourPicker::getColorValue() const  {
-    return m_rgb;
-}
-
 void cocos2d::extension::CCControlColourPicker::updateControlPicker() {
     m_huePicker->setHue(m_hsv.h);
     m_colourPicker->updateWithHSV(m_hsv);
