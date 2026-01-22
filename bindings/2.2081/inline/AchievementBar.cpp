5d2b52ea897c6dd0f60c47e78fe21efd7c6c79d1
Emi <155891536+Tyegurr@users.noreply.github.com>
2026-01-22 18:52:15 -0500

    Removed inline definition for AchievementBar::create on Windows

--- bindings/2.2081/inline/AchievementBar.cpp
+++ bindings/2.2081/inline/AchievementBar.cpp
@@ -9,18 +9,6 @@ AchievementBar::AchievementBar() {
 #if defined(GEODE_IS_WINDOWS) || defined(GEODE_IS_IOS)
 #endif
 
-#if defined(GEODE_IS_WINDOWS)
-AchievementBar* AchievementBar::create(char const* title, char const* desc, char const* icon, bool quest) {
-    auto ret = new AchievementBar();
-    if (ret->init(title, desc, icon, quest)) {
-        ret->autorelease();
-        return ret;
-    }
-    delete ret;
-    return nullptr;
-}
-#endif
-
 #if defined(GEODE_IS_IOS)
 #endif
 
