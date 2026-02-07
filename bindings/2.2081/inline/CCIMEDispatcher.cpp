638100676c963f37c627744e2e2bc5bb4d0fa55a
km7dev <100500023+Kingminer7@users.noreply.github.com>
2026-02-07 07:35:13 -0500

    Changes for iOS keyboard support (#1290)
    * Changes for iOS keyboard support
    
    * Add CCIMEDispatcher implementation for iOS
    
    ---------
    
    Co-authored-by: kingminer7 <km7@km7dev.me>
    Co-authored-by: Jasmine <52604018+hiimjasmine00@users.noreply.github.com>

--- bindings/2.2081/inline/CCIMEDispatcher.cpp
+++ bindings/2.2081/inline/CCIMEDispatcher.cpp
@@ -8,5 +8,14 @@
 #endif
 
 #if defined(GEODE_IS_IOS)
+class cocos2d::CCIMEDispatcher::Impl {
+public:
+    std::list<CCIMEDelegate*> m_DelegateList;
+    CCIMEDelegate* m_DelegateWithIme;
+};
+
+bool cocos2d::CCIMEDispatcher::hasDelegate() {
+    return m_pImpl->m_DelegateWithIme != nullptr;
+}
 #endif
 
