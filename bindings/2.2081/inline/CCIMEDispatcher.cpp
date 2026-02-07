b50b0e2296b0194f648beb1165af8554f642be79
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-02-07 07:30:14 -0500

    Add CCIMEDispatcher implementation for iOS

--- bindings/2.2081/inline/CCIMEDispatcher.cpp
+++ bindings/2.2081/inline/CCIMEDispatcher.cpp
@@ -8,6 +8,12 @@
 #endif
 
 #if defined(GEODE_IS_IOS)
+class cocos2d::CCIMEDispatcher::Impl {
+public:
+    std::list<CCIMEDelegate*> m_DelegateList;
+    CCIMEDelegate* m_DelegateWithIme;
+};
+
 bool cocos2d::CCIMEDispatcher::hasDelegate() {
     return m_pImpl->m_DelegateWithIme != nullptr;
 }
