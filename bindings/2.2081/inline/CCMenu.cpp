15a1b6e6440040a7ed8bf97afb1fd6a3655dbf02
Jasmine <52604018+hiimjasmine00@users.noreply.github.com>
2026-02-18 08:39:16 -0500

    CCMenu::create with variadic arguments

--- bindings/2.2081/inline/CCMenu.cpp
+++ bindings/2.2081/inline/CCMenu.cpp
@@ -1,5 +1,14 @@
 #include <Geode/Bindings.hpp>
 
+#if defined(GEODE_IS_MACOS) || defined(GEODE_IS_IOS)
+cocos2d::CCMenu* cocos2d::CCMenu::create(cocos2d::CCMenuItem* item, ...) {
+    va_list args;
+    va_start(args, item);
+    auto ret = cocos2d::CCMenu::createWithItems(item, args);
+    va_end(args);
+    return ret;
+}
+#endif
 
 #if defined(GEODE_IS_WINDOWS) || defined(GEODE_IS_IOS)
 #endif
