8327f1c55329fffb6c3fc8761a72e5220c656c66
thesillydoggo <talgood09@gmail.com>
2026-03-14 23:02:03 +1100

    more needed

--- bindings/2.2081/inline/CCTiledGrid3DAction.cpp
+++ bindings/2.2081/inline/CCTiledGrid3DAction.cpp
@@ -8,5 +8,43 @@
 #endif
 
 #if defined(GEODE_IS_IOS)
+using namespace cocos2d;
+
+CCGridBase* CCTiledGrid3DAction::getGrid(void)
+{
+    return CCTiledGrid3D::create(m_sGridSize);
+}
+
+ccQuad3 CCTiledGrid3DAction::tile(const CCPoint& pos)
+{
+    CCTiledGrid3D *g = (CCTiledGrid3D*)m_pTarget->getGrid();
+    return g->tile(pos);
+}
+
+ccQuad3 CCTiledGrid3DAction::originalTile(const CCPoint& pos)
+{
+    CCTiledGrid3D *g = (CCTiledGrid3D*)m_pTarget->getGrid();
+    return g->originalTile(pos);
+}
+
+void CCTiledGrid3DAction::setTile(const CCPoint& pos, const ccQuad3& coords)
+{
+    CCTiledGrid3D *g = (CCTiledGrid3D*)m_pTarget->getGrid();
+    return g->setTile(pos, coords);
+}
+
+CCTiledGrid3DAction* CCTiledGrid3DAction::create(float duration, const CCSize& gridSize)
+{
+    CCTiledGrid3DAction* pRet = new CCTiledGrid3DAction();
+    if (pRet && pRet->initWithDuration(duration, gridSize))
+    {
+        pRet->autorelease();
+    }
+    else
+    {
+        CC_SAFE_DELETE(pRet);
+    }
+    return pRet;
+}
 #endif
 
